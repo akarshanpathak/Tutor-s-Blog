0a8eb2f50bd689cf489ea385b0c8803d3a09c1ee
akarshanpathak <akarshanpathak2411@gmail.com>
2024-07-14 17:29:39 +0530

    complete build setup

--- api/index.js
+++ api/index.js
@@ -6,8 +6,10 @@ import authRouter from './routes/auth.route.js'
 import cors from "cors"
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
 import commentRoutes from './routes/comment.route.js'
 dotenv.config();
+const __dirname=path.resolve();
 const app=express();
 
 app.use(cors());
@@ -25,6 +27,10 @@ app.use('/api/post', postRoutes);
 app.use("/api/comment",commentRoutes)
 app.get("/hello")
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.listen(3000,()=>{
     console.log('Server is listening!!')
 });
